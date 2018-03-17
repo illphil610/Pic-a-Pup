e3fb3ffc0e3c086c8522202bc00d221b858f831a
Philip <pcappelli901@gmail.com>
2018-03-17 04:10:32 -0400

    Error handling with camera

--- Pic-a-Pup/Controllers/CameraViewController.swift
+++ Pic-a-Pup/Controllers/CameraViewController.swift
@@ -12,35 +12,51 @@ import CameraManager
 
 class CameraViewController : UIViewController, UIImagePickerControllerDelegate,
                                                UINavigationControllerDelegate {
-
     
     let picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         picker.delegate = self
-        
     }
     
     @IBAction func launchPhotoGallery(_ sender: UIBarButtonItem) {
         picker.allowsEditing = false
         picker.sourceType = .photoLibrary
         picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        picker.modalPresentationStyle = .popover
         present(picker, animated: true, completion: nil)
+        picker.popoverPresentationController?.barButtonItem = sender
     }
     
     @IBAction func launchCamera(_ sender: UIBarButtonItem) {
-        picker.allowsEditing = false
-        picker.sourceType = UIImagePickerControllerSourceType.camera
-        picker.cameraCaptureMode = .photo
-        picker.modalPresentationStyle = .fullScreen
-        present(picker,animated: true,completion: nil)
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.allowsEditing = false
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.cameraCaptureMode = .photo
+            picker.modalPresentationStyle = .fullScreen
+            present(picker,animated: true,completion: nil)
+        } else {
+            noCamera()
+        }
+    }
+    
+    func noCamera() {
+        let alertVC = UIAlertController(title: "No Camera",
+                                        message: "Sorry, this device has no camera",
+                                        preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK",
+                                     style:.default,
+                                     handler: nil)
+        alertVC.addAction(okAction)
+        present(alertVC, animated: true, completion: nil)
     }
     
     //MARK: - Delegates
-    func imagePickerController(_ picker: UIImagePickerController,
+    @objc func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        print("hey, \(chosenImage)")
         dismiss(animated:true, completion: nil)
     }
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
